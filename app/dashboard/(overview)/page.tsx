b605e0d4fe0d2fe4979d903db2d2ef18f784f93d
ratansingh115 <ratansingh115@gmail.com>
2025-03-02 21:20:40 +0530

    Chapter-15 authentication, metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,10 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
-import { fetchCardData } from "../../lib/data";
 
- 
 export default async function Page() {
   return (
     <main>
@@ -18,15 +15,6 @@ export default async function Page() {
         <Suspense fallback = {<CardsSkeleton/>}>
           <CardWrapper/>
         </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
-
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback ={<RevenueChartSkeleton/>}>
